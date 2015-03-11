0f328908fd7f4903634419c2b5c7cb6c08cc9503
Miriam Moser <mirjoy.moser@gmail.com>
2015-03-11 15:15:08 -0600

    made syntax changes

--- spec/users/user_visit_menu_spec.rb
+++ spec/users/user_visit_menu_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "user visits menu", type: :feature do
   it "can see all items" do
     create(:item)
-    visit '/menu'
+    visit "/menu"
     expect(page).to have_content("Salted Caramel Peanut Butter Cup")
   end
 end
