3f3b09ea401812e89ae3643cd9fa5e71652cddcc
Miriam Moser <mirjoy.moser@gmail.com>
2015-03-11 15:14:24 -0600

    more hound changes

--- spec/users/user_visit_menu_spec.rb
+++ spec/users/user_visit_menu_spec.rb
@@ -1,12 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe "user visits menu", :type => :feature do
+RSpec.describe "user visits menu", type: :feature do
   it "can see all items" do
-    user = create(:user)
-    item = create(:item)
+    create(:item)
     visit '/menu'
     expect(page).to have_content("Salted Caramel Peanut Butter Cup")
-    
   end
-
 end
