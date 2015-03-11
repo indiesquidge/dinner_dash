fd4175fa3f589039e850d4d10e07390cac62d0c3
Miriam Moser <mirjoy.moser@gmail.com>
2015-03-11 15:15:53 -0600

    added white space because hound

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,5 +1,5 @@
 class ItemOrder < ActiveRecord::Base
-  validates :quantity, numericality: {greater_than: 0}
+  validates :quantity, numericality: { greater_than: 0 }
   belongs_to :item
   belongs_to :order
 end
