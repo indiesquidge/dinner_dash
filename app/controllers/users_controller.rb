8dca5fb62185d069043a327d6674edd89b9b9de7
Austin Wood <i@austinwood.me>
2015-03-12 12:17:29 -0600

    Simplify login process, remove unneeded methods in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:create, :read, :update, :destroy]
   before_filter :authorize
 
-  def show
-    @user = User.find(params[:id])
-  end
-
   private
-
-  def set_user
-    User.find(params[:id])
-  end
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
-  end
 
   def authorize
     if current_user.nil?
