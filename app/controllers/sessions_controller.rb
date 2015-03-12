5f15b8f9e4096a80dfbe7c050f86de9f12e77220
Austin Wood <i@austinwood.me>
2015-03-12 12:19:57 -0600

    Unauth user can view account page afterlog in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
-
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to account_path, notice: "Login successful"
+    else
+      flash[:errors] = "Invalid login"
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
 
   get "/account", to: "users#show"
   get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
 end
