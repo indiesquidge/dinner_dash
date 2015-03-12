83393a62286cd4109b21502943f112d9f79f8804
Miriam Moser <mirjoy.moser@gmail.com>
2015-03-12 11:55:11 -0600

    refactored factory girl tests

--- spec/features/users/user_visits_category_spec.rb
+++ spec/features/users/user_visits_category_spec.rb
@@ -2,22 +2,17 @@ require "rails_helper"
 
 RSpec.describe "user can find things by category", type: :feature do
   it "can click on a category tag on the menu page" do
-    item = create(:item)
     category = create(:category)
-    item.categories << category
-    item.save!
+    create(:item, categories: [category])
 
     visit "/menu"
     click_link_or_button("Specials")
-    # binding.pry
     assert page.current_path == "/menu/category/specials"
   end
 
   it "can see different category pages" do
-    item = create(:item)
     category = create(:category)
-    item.categories << category
-    item.save!
+    create(:item, categories: [category])
 
     visit "/menu/category/specials"
     expect(page).to have_content("Salted Caramel Peanut Butter Cup")
