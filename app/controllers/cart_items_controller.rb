f26e019d9cd3027d8f931afc9efc1b897f32fad2
Teresa Finn <teresa.finn@gmail.com>
2015-03-12 22:08:00 -0600

    Add destroy method and flash notice so items can be removed from cart.

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -5,10 +5,19 @@ class CartItemsController < ApplicationController
 
     session[:cart] = @cart.data
     flash[:notice] = "You have #{pluralize(@cart.item_quantity(item), item.name)} in your cart."
-    redirect_to menu_path
+    redirect_to :back
   end
 
   def index
     @cart
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    @cart.remove_item(item)
+
+    session[:cart] = @cart.data
+    flash[:notice] = "The item has been removed from your cart."
+    redirect_to cart_path
   end
 end
