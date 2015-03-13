d0245eea02a0765c38d8bd0dbe65e66f2397634c
Miriam Moser <mirjoy.moser@gmail.com>
2015-03-13 08:10:30 -0600

    added double quotes

--- spec/features/auth_users/auth_user_logs_out_spec.rb
+++ spec/features/auth_users/auth_user_logs_out_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 RSpec.feature "Authenticated user", type: :feature do
   it "can logout" do
     create(:user)
+
     visit account_path
     fill_in "session[email]", with: "richard@example.com"
     fill_in "session[password]", with: "hello"
     click_link_or_button "Login"
-
     visit account_path
     click_link_or_button "Sign Out"
     within ("#flash_notice") do
