a0419d9bfb4986612132128d94fb7417b8b29fc0
Teresa Finn <teresa.finn@gmail.com>
2015-03-15 11:50:40 -0600

    More hound syntax fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,12 +6,9 @@ module ApplicationHelper
       notice: "alert-info" }[flash_class.to_sym] || flash_class.to_s
   end
 
-  def flash_messages(opts = {})
+  def flash_messages(*)
     flash.each do |msg_type, message|
-      concat(content_tag(
-        :div, message,
-        class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible",
-          role: "alert") do
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: "alert") do
         concat(content_tag(:button, class: "close", data: { dismiss: "alert" }) do
           concat content_tag(:span, "&times;".html_safe, "aria-hidden" => true)
           concat content_tag(:span, "Close", class: "sr-only")
