ce7a48fd76bb75176f393d1923bd91e3509d5bf7
Teresa Finn <teresa.finn@gmail.com>
2015-03-15 00:29:11 -0600

    Add create action to items controller so admin can create new items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,20 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find_by(parameterized_name: params[:item_name])
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+      flash[:notice] = "New item has been created!"
+      redirect_to menu_item_path(@item)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
 end
