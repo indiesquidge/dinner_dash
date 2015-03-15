23806a96df013fd87b180eaadabcb7f9127ed591
Teresa Finn <teresa.finn@gmail.com>
2015-03-15 12:00:15 -0600

    Fixed failing test.

--- spec/features/auth_users/user_visits_admin_page_spec.rb
+++ spec/features/auth_users/user_visits_admin_page_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Authenticated user", type: :feature do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     visit admin_dashboard_path
-    within ("#flash_alert") do
+    within ("div.alert-warning") do
       expect(page).to have_content("Unauthorized")
     end
     expect(page).to have_content("Menu")
