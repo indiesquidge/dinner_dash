906ec402ffc15c0dabbdcdc097567ff785978edb
Miriam Moser <mirjoy.moser@gmail.com>
2015-03-16 13:14:56 -0600

    added custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'errors/file_not_found'
+
+  get 'errors/unprocessable'
+
+  get 'errors/internal_server_error'
+
   root "menu#index"
 
   get "/menu", to: "menu#index"
@@ -18,4 +24,8 @@ Rails.application.routes.draw do
 
   resources :cart_items, only: [:create, :destroy]
   get "/cart", to: "cart_items#index"
+  
+  match '/404', to: 'errors#file_not_found', via: :all
+  match '/422', to: 'errors#unprocessable', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 end
