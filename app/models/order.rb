1a9da2ad3567897a73f93d1c38d6b83da1d52c60
Teresa Finn <teresa.finn@gmail.com>
2015-03-16 22:59:48 -0600

    Add scopes to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,11 @@ class Order < ActiveRecord::Base
   has_many :items, through: :item_orders
   belongs_to :user
 
+  scope :ordered, -> { where(status: "ordered") }
+  scope :paid, -> { where(status: "paid") }
+  scope :cancelled, -> { where(status: "cancelled") }
+  scope :completed, -> { where(status: "completed") }
+
   def total_price
     items.inject(0) { |sum, item| sum + item.price }
   end
