53d9574aa851936f973073476f05189f14f3e4be
Teresa Finn <teresa.finn@gmail.com>
2015-03-17 01:47:52 -0600

    Add actions to change statuses.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,31 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
   end
 
+  def cancel
+    @order = Order.find(params[:id])
+    @order.status = "cancelled"
+    @order.save
+    flash[:notice] = "Order cancelled."
+    redirect_to :back
+  end
+
+  def paid
+    @order = Order.find(params[:id])
+    @order.status = "paid"
+    @order.save
+    flash[:notice] = "Order marked as paid."
+    redirect_to :back
+  end
+
+  def complete
+    @order = Order.find(params[:id])
+    @order.status = "completed"
+    @order.save
+    flash[:notice] = "Order marked as completed."
+    redirect_to :back
+  end
+
+
   # def new
   #   @category = Category.new
   #   authorize! :new, @category
