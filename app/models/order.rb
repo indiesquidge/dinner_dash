2ab0ce33cd7fd654cb09fabac2f1f49a8584f916
Austin Wood <i@austinwood.me>
2015-03-17 14:36:00 -0600

    Move duplicate methods to application controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -13,10 +13,6 @@ class Order < ActiveRecord::Base
     items.inject(0) { |sum, item| sum + item.price }
   end
 
-  def quantity_of_item(item, order)
-    ItemOrder.where(item_id: item.id, order_id: order.id)[0].quantity
-  end
-
   def line_item_price(item, order)
     item.price * quantity_of_item(item, order)
   end
