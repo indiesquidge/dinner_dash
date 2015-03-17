7b8e532be37d7ae6f74618975aadb8492a16d99f
Teresa Finn <teresa.finn@gmail.com>
2015-03-17 00:27:17 -0600

    Add orders controller.

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,31 @@
+class OrdersController < ApplicationController
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  # def new
+  #   @category = Category.new
+  #   authorize! :new, @category
+  # end
+
+  # def create
+  #   @category = Category.create(category_params)
+  #   if @category.save
+  #     flash[:success] = "New category has been created!"
+  #     redirect_to menu_categories_path
+  #   else
+  #     flash[:error] = "#{@category.errors.full_messages.first}"
+  #     redirect_to new_menu_category_path
+  #   end
+  # end
+
+
+  # def index
+  # end
+
+  # private
+
+  # def category_params
+  #   params.require(:category).permit(:name)
+  # end
+end
