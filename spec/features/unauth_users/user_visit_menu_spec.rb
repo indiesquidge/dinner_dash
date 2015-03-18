e3c866d9cb95d3cea5f34d64542e94a0237714f0
Miriam Moser <mirjoy.moser@gmail.com>
2015-03-18 09:35:20 -0600

    centered pics

--- spec/features/unauth_users/user_visit_menu_spec.rb
+++ spec/features/unauth_users/user_visit_menu_spec.rb
@@ -1,9 +1,14 @@
 require "rails_helper"
 
-RSpec.describe "user visits menu", type: :feature do
-  it "can see all items" do
+RSpec.describe "user visits website", type: :feature do
+  it "can see all items on the menu page" do
     create(:item)
     visit "/menu"
     expect(page).to have_content("Salted Caramel Peanut Butter Cup")
+  end
+
+  it "can see store info on the home page" do
+    visit '/'
+    expect(page).to have_content("Hours")
   end
 end
