3d0a16416c12f15a31f0d0dc4ca80d29be13dda9
Miriam Moser <mirjoy.moser@gmail.com>
2015-03-18 21:39:16 -0600

    made hound changes

--- spec/features/unauth_users/user_manages_cart_spec.rb
+++ spec/features/unauth_users/user_manages_cart_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "user managing cart", type: :feature do
   it "views their cart" do
     create_item_and_add_to_cart
     visit cart_path
-    expect(page).to have_content("salted caramel peanut butter cup")
+    expect(page).to have_content("Salted Caramel Peanut Butter Cup")
   end
 
   it "sees the correct quantity for a specific item" do
@@ -24,13 +24,13 @@ RSpec.describe "user managing cart", type: :feature do
     visit cart_path
     page.click_link('', :href => '/cart_items/1')
     expect(page).to have_content("The item has been removed from your cart")
-    expect(page).to_not have_content("salted caramel")
+    expect(page).to_not have_content("Salted Caramel")
   end
 
   it "allows users to increase the quantity of an item in their cart" do
     create_item_and_add_to_cart
     visit cart_path
-    page.click_link('', :href => '/cart_items?item=salted-caramel-peanut-butter-cup')
+    click_link("", :href => "/cart_items?item=salted-caramel-peanut-butter-cup")
     expect(page).to have_content("Quantity: 2")
     expect(page).to have_content("12.00")
   end
@@ -38,7 +38,7 @@ RSpec.describe "user managing cart", type: :feature do
   it "allows user to delete all of one item at once" do
     create_item_and_add_to_cart
     visit cart_path
-    page.click_link('', :href => '/cart_items?item=salted-caramel-peanut-butter-cup')
+    click_link("", :href => "/cart_items?item=salted-caramel-peanut-butter-cup")
     click_link_or_button("Remove All")
     expect(page).to have_content("Total Items: 0")
   end
