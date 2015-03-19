1d00759843c16a31f3c2f350ddaa213cb8fedc8a
Miriam Moser <mirjoy.moser@gmail.com>
2015-03-18 21:42:58 -0600

    misunderstood hound messages - even more hound changes

--- spec/features/unauth_users/user_manages_cart_spec.rb
+++ spec/features/unauth_users/user_manages_cart_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe "user managing cart", type: :feature do
   it "allows users to remove things from their cart" do
     create_item_and_add_to_cart
     visit cart_path
-    click_link('', :href => '/cart_items/1')
+    click_link('', href: '/cart_items/1')
     expect(page).to have_content("The item has been removed from your cart")
     expect(page).to_not have_content("Salted Caramel")
   end
@@ -30,7 +30,7 @@ RSpec.describe "user managing cart", type: :feature do
   it "allows users to increase the quantity of an item in their cart" do
     create_item_and_add_to_cart
     visit cart_path
-    click_link("", :href => "/cart_items?item=salted-caramel-peanut-butter-cup")
+    click_link("", href: "/cart_items?item=salted-caramel-peanut-butter-cup")
     expect(page).to have_content("Quantity: 2")
     expect(page).to have_content("12.00")
   end
@@ -38,7 +38,7 @@ RSpec.describe "user managing cart", type: :feature do
   it "allows user to delete all of one item at once" do
     create_item_and_add_to_cart
     visit cart_path
-    click_link("", :href => "/cart_items?item=salted-caramel-peanut-butter-cup")
+    click_link("", href: "/cart_items?item=salted-caramel-peanut-butter-cup")
     click_link_or_button("Remove All")
     expect(page).to have_content("Total Items: 0")
   end
