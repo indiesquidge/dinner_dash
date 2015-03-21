27d327347a58921b0ed5d3978d53bfbb2801a8d0
Josh Cheek <josh.cheek@gmail.com>
2015-03-21 16:33:44 -0600

    Very questionable way to make phantom work with Capybara
    The issue is that when Phantomjs makes the request to our server,
    the server uses a different connection, which is outside of the
    transaction that DatabaseCleaner has wrapped around the test.
    So it cannot see the test data that we inserted, and possibly gets
    put to sleep waiting for the transaction to end.
    
    This change causes the server to use the same connection
    as the tests, so it can see within the transaction.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,3 +14,13 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
 end
+
+# DATABASE CLEANER
+  # This just screams danger in so many ways
+  # Taken regretfully from https://gist.github.com/josevalim/470808
+  class ActiveRecord::Base
+    @@shared_connection = retrieve_connection
+    def self.connection
+      @@shared_connection || retrieve_connection
+    end
+  end
